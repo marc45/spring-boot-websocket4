56bf3ae0dc87e9980e29dc543d7d046d1b4d55e9
surgelt <surge_lt@163.com>
2017-04-26 17:12:58 +0800

    commit1

---
+++ src/main/java/com/learn/springboot/websocket/Message.java
@@ -0,0 +1,17 @@
+package com.learn.springboot.websocket;
+
+/**
+ * Created by liutao on 17/4/26.
+ */
+public class Message {
+	@Override
+	public String toString() {
+		return "Message [name=" + name + "]";
+	}
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+}
